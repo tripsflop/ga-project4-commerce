25e6b6df20e7639f44fc33712f62ebbf707fb209
Lim Zheng Kai <zhengkai@Lims-MacBook-Air.local>
2023-01-26 22:08:48 +0800

    added css to cart page

--- client/src/components/routes/Cart.jsx
+++ client/src/components/routes/Cart.jsx
@@ -49,31 +49,112 @@ function Cart() {
   };
 
   return (
-    <section>
-      <h1>Cart</h1>
-      {cart.products.map((product, index) => (
-        <div key={index}>
-          <img
-            src={product.thumbnail}
-            alt={product.urlKey}
-            style={{ width: "40%", height: "40%" }}
-          />
-          <div>{product.shoeName}</div>
-          <div>Size:{product.size}</div>
-          <div>${product.retailPrice}</div>
-          <div>Quantity:{product.quantity}</div>
-          <div>
-            <button value={index} onClick={handleIncrease}>
-              +
-            </button>
-            <button value={index} onClick={handleDecrease}>
-              -
-            </button>
+    <section className="h-100 mt-5 h-custom">
+      <div className="container h-100 py-5">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col">
+            <div className="table-responsive">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col" className="h5">
+                      Item
+                    </th>
+                    <th scope="col">Size</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Unit Price</th>
+                  </tr>
+                  {cart.products.map((product, index) => (
+                    <tr key={index}>
+                      <th scope="row">
+                        <div className="d-flex align-items-center">
+                          <img
+                            src={product.thumbnail}
+                            alt={product.urlKey}
+                            className="img-fluid rounded-3"
+                            style={{ width: 120 }}
+                          />
+                          <div className="flex-column ms-5 hide-desc">
+                            <p className="mb-2">{product.brand}</p>
+                            <p className="mb-0">{product.shoeName}</p>
+                          </div>
+                        </div>
+                      </th>
+                      <td className="align-middle">
+                        <p className="mb-0">{product.size}</p>
+                      </td>
+                      <td className="align-middle">
+                        <div className="d-flex flex-row">
+                          <button
+                            className="btn btn-outline-dark"
+                            value={index}
+                            onClick={handleDecrease}
+                          >
+                            -
+                          </button>
+
+                          <div className="mx-2 my-2">{product.quantity}</div>
+
+                          <button
+                            className="btn btn-outline-dark"
+                            value={index}
+                            onClick={handleIncrease}
+                          >
+                            +
+                          </button>
+                        </div>
+                      </td>
+                      <td className="align-middle">
+                        <p className="mb-0">${product.retailPrice}</p>
+                      </td>
+                    </tr>
+                  ))}
+                </thead>
+                <tbody></tbody>
+              </table>
+            </div>
+
+            <div
+              className="card shadow-2-strong mb-5 mb-lg-0"
+              style={{ borderRadius: 16 }}
+            >
+              <div className="card-body p-4">
+                <div className="row justify-content-end">
+                  <div className="col-lg-4 col-xl-3">
+                    <div className="d-flex justify-content-between">
+                      <p className="mb-2">Subtotal</p>
+                      <p className="mb-2">${cart.total}</p>
+                    </div>
+
+                    <div className="d-flex justify-content-between">
+                      <p className="mb-0">Shipping</p>
+                      <p className="mb-0 text-muted">To be calculated</p>
+                    </div>
+
+                    <hr className="my-4" />
+
+                    <div className="d-flex justify-content-between mb-4">
+                      <p className="mb-2">Total (tax included)</p>
+                      <p className="mb-2">${cart.total}</p>
+                    </div>
+                    <div className="d-flex justify-content-end">
+                      <button
+                        type="button"
+                        onClick={handleCheckout}
+                        className="btn btn-dark btn-lg"
+                      >
+                        <div className="d-flex justify-content-between">
+                          <span>Checkout</span>
+                        </div>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      ))}
-      <div>Total:${cart.total}</div>
-      <button onClick={handleCheckout}>Checkout</button>
+      </div>
     </section>
   );
 }
